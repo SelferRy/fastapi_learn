636c860dcc592c01d3e6a0a9d3bf4f088df1eeb7
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2023-04-27 00:17:05 +0300

    pydantic validation

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
-from fastapi import FastAPI
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI(
     title="Trading App"
 )
 
 
-# @app.get("/")
-# def hello():
-#     return "Hello world! With something new."
+@app.get("/")
+def hello():
+    return "Hello world! With something new."
 
 
 fake_users = [
-    {"id": 1, "role": "admin", "name": "Bob"},
+    {"id": 1, "role": ["admin"], "name": "Bob"},
     {"id": 2, "role": "investor", "name": "John"},
     {"id": 3, "role": "trader", "name": "Matt"},
+    {"id": 4, "role": "investor", "name": "Homer", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]}
 ]
 
 
-@app.get("/user/{user_id}")
+class DegreeType(Enum):
+    newbie = "mewbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []
+
+
+@app.get("/user/{user_id}", response_model=List[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
@@ -39,3 +64,18 @@ def change_name(user_id: int, new_name: str):
     current_user = [user for user in fake_users if user.get("id") == user_id][0]
     current_user["name"] = new_name
     return {"status": 200, "data": current_user}
+
+
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=5)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
