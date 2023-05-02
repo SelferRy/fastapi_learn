090209afce0a25dc47e0b6b7427dbb3b1a7000b6
Vladimir Selifanov <vladimir.v.selifanov@gmail.com>
2023-05-02 23:11:34 +0300

    Filled User + strictly defined the class variables (copy from superclass)

--- auth/database.py
+++ auth/database.py
@@ -1,20 +1,41 @@
+from datetime import datetime
 from typing import AsyncGenerator
 
 from fastapi import Depends
 from fastapi_users.db import SQLAlchemyUserDatabase
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import String, Boolean, Column, Integer, TIMESTAMP, ForeignKey
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Mapped, mapped_column
 
 from config import DB_USER, DB_PASS, DB_HOST, DB_PORT, DB_NAME
+from models.models import role
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 Base: DeclarativeMeta = declarative_base()
 
 
 class User(SQLAlchemyBaseUserTable[int], Base):
-    pass
+    id = Column("id", Integer, primary_key=True),
+    email = Column("email", String, nullable=False),
+    username = Column("username", String, nullable=False),
+    password = Column("password", String, nullable=False),
+    registered_at = Column("registered_at", TIMESTAMP, default=datetime.utcnow),
+    role_id = Column("role_id", Integer, ForeignKey(role.c.id)),
+    email: Mapped[str] = mapped_column(
+        String(length=320), unique=True, index=True, nullable=False
+    )
+    hashed_password: Mapped[str] = mapped_column(
+        String(length=1024), nullable=False
+    )
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True, nullable=False)
+    is_superuser: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
+    is_verified: Mapped[bool] = mapped_column(
+        Boolean, default=False, nullable=False
+    )
 
 
 engine = create_async_engine(DATABASE_URL)
